b06f947a33a7b3920873db6c776710a2a2745cfd
Gisle Datahjelpenit <gisle@datahjelpenit.no>
2026-06-17 13:53:12 +0200

    Vite testproject react app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router";
+import { SignUpPage } from './SignUpPage';
+import { LogInPage } from './LogInPage';
+import { UserInfoPage } from './UserInfoPage';
+
+function App() {
+  return (
+    <div className="page-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/log-in" element={<LogInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/" element={<UserInfoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
